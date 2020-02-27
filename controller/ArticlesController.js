3ac707f12cc2e85c2faf40b22d020a2f0e586b06
1gilmar <1gilmar@gmail.com>
2020-02-27 15:57:14 -0300

    criado a rota save e index

--- controller/ArticlesController.js
+++ controller/ArticlesController.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose")
 require("../models/Category")
 const Categoria = mongoose.model("categories")
+require("../models/Article")
+const Artigo = mongoose.model("articles")
+
+const slugifly = require("slugify")
 
 
 //abrir o formulario novo
@@ -11,4 +15,30 @@ exports.new = (req, res) =>{
     }).catch((erro)=>{
         res.send("Erro ao carregar as categorias")
     })
+}
+
+exports.save = (req, res) =>{
+    //pegando os name do formulario para salvar o artigo
+    var titulo = req.body.title
+    var corpo = req.body.body
+    var categoria = req.body.category
+    var descricao = req.body.title
+
+    const novoArtigo = {
+        title: titulo,
+        body: corpo,
+        slug: slugifly(titulo),
+        descripton: descricao,
+        category: categoria,
+    }
+
+    new Artigo(novoArtigo).save().then(()=>{
+        res.redirect("/admin/artigos")
+    }).catch((erro) =>{
+        res.send("erro ao salvar " + erro)
+    })
+}
+
+exports.index = (req, res) =>{
+    res.render("admin/article/index")
 }
--- router/articleRoutes.js
+++ router/articleRoutes.js
@@ -3,5 +3,7 @@ const rotas = express.Router()
 const controle = require("../controller/ArticlesController")
 
 rotas.get("/artigos/new",  controle.new)
+rotas.post("/artigos/save", controle.save)
+rotas.get("/artigos/", controle.index)
 
 module.exports = rotas
